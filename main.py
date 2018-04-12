655e1fc1e90ef68078be46922ddf0db62a2174e7
Sean Grogan <sean.grogan@gmail.com>
2018-04-12 15:04:35 -0400

    Added a new test that will test a random policy against a set of random policies

--- main.py
+++ main.py
@@ -1,11 +1,80 @@
 from player import *
-import math
 
 
 def main():
-    # runs the default 
+    # runs the default
     default_types()
     experiment_between_def_types()
+
+
+def random_search_for_better_policy(n_test=10000, n_opp=10000):
+    """
+    This function will test a random policy against another random policy
+    :param n_test: Number of policies to test
+    :param n_opp: Number of opposition policies to test
+    :return:
+    """
+    most_wins = dict()
+    most_wins["policy"] = [0, 0, 0]
+    most_wins["win"] = 0
+    most_wins["tie"] = 0
+    most_wins["avg"] = 0
+    most_ties = dict()
+    most_ties["policy"] = [0, 0, 0]
+    most_ties["win"] = 0
+    most_ties["tie"] = 0
+    most_ties["avg"] = 0
+    best_vals = dict()
+    best_vals["policy"] = [0, 0, 0]
+    best_vals["win"] = 0
+    best_vals["tie"] = 0
+    best_vals["avg"] = 0
+
+    for i in range(n_test):
+        print(" Executing Test", i)
+        win = 0
+        tie = 0
+        val = list()
+        f1 = random.randint(0, 100) / 100.0
+        i1 = random.randint(0, 100) / 100.0
+        v1 = random.randint(0, 100) / 100.0
+        p1_pol = [f1, i1, v1]
+        for i in range(n_opp):
+            f2 = random.randint(0, 100) / 100.0
+            i2 = random.randint(0, 100) / 100.0
+            v2 = random.randint(0, 100) / 100.0
+            p2_pol = [f2, i2, v2]
+            p1v, p2v = experiment(p1_pol, p2_pol)
+            val.append(p1v)
+            if p1v > p2v:
+                win += 1
+            if p1v == p2v:
+                tie += 1
+        avg = sum(val)/n_opp
+        if win > most_wins["win"]:
+            most_wins["policy"] = p1_pol.copy()
+            most_wins["win"] = win
+            most_wins["tie"] = tie
+            most_wins["avg"] = avg
+            print("  Most Wins  ", most_wins["policy"], most_wins["win"], most_wins["tie"], most_wins["avg"])
+        if tie > most_ties["tie"]:
+            most_ties["policy"] = p1_pol.copy()
+            most_ties["win"] = win
+            most_ties["tie"] = tie
+            most_ties["avg"] = avg
+            print("  Most Ties  ", most_ties["policy"], most_ties["win"], most_ties["tie"], most_ties["avg"])
+        if avg > best_vals["avg"]:
+            best_vals["policy"] = p1_pol.copy()
+            best_vals["win"] = win
+            best_vals["tie"] = tie
+            best_vals["avg"] = avg
+            print("  Best Value ", best_vals["policy"], best_vals["win"], best_vals["tie"], best_vals["avg"])
+
+    print()
+    print("Most Wins  ", most_wins["policy"], most_wins["win"], most_wins["tie"], most_wins["avg"])
+    print("Most Ties  ", most_ties["policy"], most_ties["win"], most_ties["tie"], most_ties["avg"])
+    print("Best Value ", best_vals["policy"], best_vals["win"], best_vals["tie"], best_vals["avg"])
+
 
 def default_types():
     pol1 = [1, 0, 0]  # Cooperator
@@ -33,7 +102,12 @@ def default_types():
     experiments(pol6)
     print()
 
+
 def experiment_between_def_types():
+    """
+    Does the experiment with different policies, specifically the default types in the
+    :return:
+    """
     pol1 = [1, 0, 0]  # Cooperator
     pol2 = [0.5, 0, 0]  # Coin Flipper
     pol3 = [1, 1, 0]  # Copycat
@@ -56,12 +130,13 @@ def experiment_between_def_types():
         print("Ties : ", tie)
         print()
 
+
 def experiments(p1, n=10000):
     """
     Runs n experiemts given a player 1 value
     :param p1: list of [f, i, v]
     :param n: number of sims
-    :return:
+    :return: tuple of number of wins and ties
     """
     win = 0
     tie = 0
@@ -78,6 +153,7 @@ def experiments(p1, n=10000):
             tie += 1
     print("Wins : ", win, "(", int(win*100/n), "%)")
     print("Ties : ", tie, "(", int(tie*100/n), "%)")
+    return win, tie
 
 
 def experiment(player1=[1.0, 0.5, 0.25], player2=[1.0, 0.5, 0.25], c_c=15, d_d=10, c_d=5, d_c=25, n=100):
@@ -129,4 +205,5 @@ def experiment(player1=[1.0, 0.5, 0.25], player2=[1.0, 0.5, 0.25], c_c=15, d_d=1
 
 
 if __name__ == '__main__':
-    main()
+    random_search_for_better_policy(100, 1000)
+    # main()
